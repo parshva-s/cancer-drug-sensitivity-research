6d3456b671c566998a49c3d582dfb399136f4a8f
parshva-s <parshva.s24@gmail.com>
2024-10-09 09:24:53 -0600

    getter for feature names

--- Dataset.py
+++ Dataset.py
@@ -1,7 +1,16 @@
 class Dataset:
-    def __init__(self, gene_file, IC50_file = None):
+    def __init__(self, gene_file, IC50_file=None):
         self.features = None
         self.targets = None
         self.feature_names = None
-        
-        self.feature_size = {"x" : None, "y" : None}
+
+        self.feature_size = {"x": None, "y": None}
+
+    def get_feature_names(self) -> list:
+        """get names of features
+
+        Returns:
+            list: feature names
+        """
+        return self.feature_names
+
