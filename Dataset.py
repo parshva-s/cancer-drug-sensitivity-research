4129ff5def4e2373230de32bd563afa28485f167
parshva-s <parshva.s24@gmail.com>
2024-10-09 18:25:42 -0600

    pep8

--- Dataset.py
+++ Dataset.py
@@ -2,18 +2,27 @@ import pandas as pd
 import numpy as np
 import os
 
+
 class Dataset:
-    def __init__(self, gene_file : str = None, IC50_file : str = None, data_directory : str =None):
+    def __init__(
+            self,
+            gene_file: str = None,
+            IC50_file: str = None,
+            data_directory: str = None):
         self.features = None
         self.targets = None
         self.feature_names = None
-        
+
         self.gene_expression_data = None
+        self.drug_cell_line_data = None
 
         self.feature_size = {"x": None, "y": None}
-        
+
         if gene_file is not None and data_directory is not None:
             self.set_features(gene_file, data_directory)
+
+        if IC50_file is not None and data_directory is not None:
+            self.set_targets(IC50_file, data_directory)
 
     def get_feature_names(self) -> list:
         """get names of features
@@ -36,8 +45,9 @@ class Dataset:
             dict: the x and y dimensions of feature space
         """
         return self.feature_size
-    
-    def set_features_GDSC(self, feature_file_name : str, data_directory : str) -> None:
+
+    def set_features_GDSC(self, feature_file_name: str,
+                          data_directory: str) -> None:
         """set features from a file
 
         Args:
@@ -45,19 +55,38 @@ class Dataset:
             data_directory (str): the directory which feature file is in
         """
         if not self.check_valid_file(feature_file_name, data_directory):
-            return 
-        
-        data = pd.read_csv(data_directory + feature_file_name)
-        
-        grouped_data = data.groupby(['Cell Line Name', 'Genes in Segment'], as_index=False).agg({'IS Mutated': 'max'})
-        pivoted_data = grouped_data.pivot(index='Cell Line Name', columns='Genes in Segment', values='IS Mutated')
+            return
 
-        # Fill any missing values with 0 (if any genes are missing for certain cell lines)
+        data = pd.read_csv(data_directory + feature_file_name)
+
+        grouped_data = data.groupby(
+            ['Cell Line Name', 'Genes in Segment'], as_index=False).agg({'IS Mutated': 'max'})
+        pivoted_data = grouped_data.pivot(
+            index='Cell Line Name',
+            columns='Genes in Segment',
+            values='IS Mutated')
+
+        # Fill any missing values with 0 (if any genes are missing for certain
+        # cell lines)
         pivoted_data = pivoted_data.fillna(0)
 
         # Convert the data to integer type (from float if NaN was present)
         self.gene_expression_data = pivoted_data.astype(int)
-    def check_valid_file(file_name : str, data_directory : str) -> bool:
+
+        # get list  of data headers
+        self.feature_names = self.gene_expression_data.columns
+
+    def set_targets_GDSC(self, target_file: str, data_directory: str) -> None:
+        """set targets from a file
+
+        Args:
+            feature_file_name (str): the name of the file to read features from.
+            data_directory (str): the directory which feature file is in
+        """
+        if not self.check_valid_file(target_file, data_directory):
+            return
+
+    def check_valid_file(file_name: str, data_directory: str) -> bool:
         """checks if a file is valid
 
         Args:
@@ -70,18 +99,21 @@ class Dataset:
         if file_name is None:
             print("No file name provided")
             return False
-        
+
         if not os.path.exists(data_directory + file_name):
             print("File does not exist")
             return False
-        
+
         if not file_name.endswith(".csv"):
             print("File is not a csv file")
             return False
-        
+
         return True
-    
-    def gene_data_to_csv(self, data_directory : str, output_file_name : str = None) -> None:
+
+    def gene_data_to_csv(
+            self,
+            data_directory: str,
+            output_file_name: str = None) -> None:
         """creates a csv file from gene expression data
 
         Args:
@@ -92,12 +124,17 @@ class Dataset:
             if not output_file_name.endswith(".csv"):
                 output_file_name += ".csv"
             self.gene_expression_data.to_csv(data_directory + output_file_name)
-            print("Gene Expression data saved in: " + data_directory + output_file_name)
+            print(
+                "Gene Expression data saved in: " +
+                data_directory +
+                output_file_name)
         else:
-            self.gene_expression_data.to_csv(data_directory + "gene_expression_out.csv") 
-        
+            self.gene_expression_data.to_csv(
+                data_directory + "gene_expression_out.csv")
+
+
 if __name__ == "__main__":
     data_directory = "data/"
     dataset = Dataset("GDSC")
     dataset.set_features_GDSC("gene_expression.csv", data_directory)
-    dataset.gene_data_to_csv(data_directory)
+    # dataset.gene_data_to_csv(data_directory)
