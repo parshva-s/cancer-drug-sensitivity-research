d44f65a547b4ce3a65d05855036eed44b65de92b
parshva-s <parshva.s24@gmail.com>
2024-10-31 17:55:08 -0600

    contructor

--- preprocessor/DrugSpecificDataset.py
+++ preprocessor/DrugSpecificDataset.py
@@ -1,4 +1,16 @@
 from Dataset import Dataset
 
 class DrugSpecificDataset():
-    
+    def __init__(self,
+        drug_id: str = None,
+        features: list = None,
+        targets: list = None):
+        
+        self.drug_id = drug_id
+
+        # dataset parameters
+        self.feature_names = None
+        self.features = None
+        self.targets = None
+
+        self.dataset = None
