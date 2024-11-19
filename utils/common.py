9777186a1dac18cebfaf0b8ec1b801044ca50bd2
parshva-s <parshva.s24@gmail.com>
2024-11-18 23:37:34 -0700

    making splits usable for data already given

--- utils/common.py
+++ utils/common.py
@@ -17,9 +17,21 @@ def load_and_prepare_data(file_path: str, drop_columns: list) -> pd.DataFrame:
     return df
 
 
-def split_data(df: pd.DataFrame, top_features: list, target_variable: str, test_size=0.2):
-    X = df[top_features]
-    y = df[target_variable]
+def split_data(df: pd.DataFrame = None, top_features: list = None, target_variable: str = None, X = None, y = None, test_size=0.2):
+    
+    # check if top_features is provided
+    if top_features is not None:
+        X = df[top_features]
+    elif X is None:
+        raise ValueError("Either top_features or X should be provided.")
+    
+    # check if target_variable is provided
+    if target_variable is not None: 
+        y = df[target_variable]
+    elif y is None:
+        raise ValueError("Either target_variable or y should be provided.")
+        
+    # train-test split
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=test_size, random_state=42)
     
@@ -27,8 +39,10 @@ def split_data(df: pd.DataFrame, top_features: list, target_variable: str, test_
     X_val, X_test, y_val, y_test = train_test_split(
         X_test, y_test, test_size=0.5, random_state=42)
     
+    # scaling the data
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X_train)
     X_val_scaled = scaler.transform(X_val)
     X_test_scaled = scaler.transform(X_test)
+    
     return X_train_scaled, X_val_scaled, X_test_scaled, y_train, y_val, y_test
